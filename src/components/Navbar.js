db62827bb23d79c1e278a6c4bdaa8da6f6811bee
tomu <fukuda.receives.email@gmail.com>
2021-07-21 17:11:49 +0900

    add about2 page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
+              <Link className="navbar-item" to="/about2">
+                ABOUT
+              </Link>
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
